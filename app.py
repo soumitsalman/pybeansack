851ecbbab929f47892b43a5bdb4d6357fa2973b6
Soumit Salman Rahman <147952840+soumitsalman@users.noreply.github.com>
2024-10-24 20:07:17 +0000

    temporarily making app.py as the holder of api interface azure multi-container web app doesnt route to the other endpoint

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import logging
 import os
 import time
-from fastapi import Query
+from fastapi import HTTPException, Query
 from authlib.integrations.starlette_client import OAuth
 from starlette.requests import Request
 from starlette.responses import RedirectResponse
@@ -9,18 +9,25 @@ from nicegui import app, ui
 from fastapi.responses import FileResponse, Response
 from icecream import ic
 import env
+from pybeansack.datamodels import *
 from shared import config
+
 ##### LOGGING SETUP SECTION #####
 
+logging.basicConfig(level=logging.WARNING, format='%(asctime)s|%(name)s|%(levelname)s|%(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+app_logger: logging.Logger = config.create_logger("__APP__", '%(asctime)s|%(name)s|%(levelname)s|%(message)s|%(user_id)s|%(page_id)s|%(q)s|%(acc)s|%(tag)s|%(kind)s|%(ndays)s', "espresso-app.log")
+api_logger: logging.Logger = config.create_logger("__API__", '%(asctime)s|%(name)s|%(levelname)s|%(message)s|%(user_id)s|%(q)s|%(acc)s|%(url)s|%(tag)s|%(kind)s|%(source)s|%(ndays)s|%(start)s|%(limit)s|%(num_items)s', "espresso-api.log")
 
-def log(function, **kwargs): 
+def log_app(function, **kwargs): 
     user = logged_in_user()
     extra = {"user_id": user[espressops.ID] if user else None, "page_id": None, "q": None, "acc": None, "url": None, "tag": None, "kind": None, "ndays": None}
     extra.update(kwargs)
-    logger.info(function, extra=extra)
+    app_logger.info(function, extra=extra)
 
-logging.basicConfig(level=logging.WARNING, format='%(asctime)s|%(name)s|%(levelname)s|%(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-logger: logging.Logger = config.create_logger("__APP__", '%(asctime)s|%(name)s|%(levelname)s|%(message)s|%(user_id)s|%(page_id)s|%(q)s|%(acc)s|%(tag)s|%(kind)s|%(ndays)s')
+def log_api(function, **kwargs):    
+    extra = {"user_id": None, "q": None, "acc": None, "url": None, "tag": None, "kind": None, "source": None, "ndays": None, "start": None, "limit": None, "num_items": None}
+    extra.update(kwargs)
+    api_logger.info(function, extra=extra)
 
 ##### SLACK APP SECTION #####
 from slack_bolt.adapter.fastapi import SlackRequestHandler
@@ -121,14 +128,14 @@ def _redirect_after_auth(name, id, image_url, source, token):
     if current_user:
         espressops.add_connection(current_user, authenticated_user)
         current_user[espressops.CONNECTIONS][source]=name
-        log('connection added')
+        log_app('connection added')
         return RedirectResponse(last_page())
         
     # if no user is logged in but there is an registered user with this cred then log-in that user    
     registered_user = espressops.get_user(authenticated_user)
     if registered_user:
         set_logged_in_user(registered_user)
-        log('logged in')
+        log_app('logged in')
         return RedirectResponse(last_page()) 
 
     set_temp_user(authenticated_user)
@@ -136,7 +143,7 @@ def _redirect_after_auth(name, id, image_url, source, token):
 
 @app.get('/logout')
 def logout():
-    log('logged out')
+    log_app('logged out')
     log_out_user()
     return RedirectResponse(last_page())
 
@@ -153,7 +160,7 @@ async def login_failed(source: str):
 
 @ui.page('/user-registration')
 def user_registration():
-    log('user_registration', user_id=temp_user()[espressops.ID] if temp_user() else None)
+    log_app('user_registration', user_id=temp_user()[espressops.ID] if temp_user() else None)
     web_ui.pages.render_user_registration(
         session_settings(), 
         temp_user(),
@@ -164,7 +171,7 @@ def user_registration():
 def home():  
     settings = session_settings()
     settings['last_page'] = "/" 
-    log('home')
+    log_app('home')
     web_ui.pages.render_home(settings, logged_in_user())
 
 @ui.page("/search")
@@ -177,7 +184,7 @@ def search(
 
     settings = session_settings()
     settings['last_page'] = web_ui.renderer.make_navigation_target("/search", q=q, tag=tag, kind=kind, ndays=ndays, acc=acc)
-    log('search', q=q, tag=tag, kind=kind, ndays=ndays, acc=acc)
+    log_app('search', q=q, tag=tag, kind=kind, ndays=ndays, acc=acc)
     web_ui.pages.render_search(settings, logged_in_user(), q, acc, tag, kind, ndays)
 
 @ui.page("/page/{category}")
@@ -189,7 +196,7 @@ def trending(
     
     settings = session_settings()
     settings['last_page'] = web_ui.renderer.make_navigation_target(f"/page/{category}", ndays=ndays) 
-    log('page', page_id=category, ndays=ndays)
+    log_app('page', page_id=category, ndays=ndays)
     web_ui.pages.render_trending(settings, logged_in_user(), category.lower(), ndays)
 
 @ui.page("/channel/{userid}")
@@ -201,7 +208,7 @@ def user_channel(
     
     settings = session_settings()
     settings['last_page'] = web_ui.renderer.make_navigation_target(f"/channel/{userid}", ndays=ndays) 
-    log('channel', page_id=userid, ndays=ndays)
+    log_app('channel', page_id=userid, ndays=ndays)
     web_ui.pages.render_user_channel(settings, logged_in_user(), userid, ndays)
 
 @ui.page("/docs/{doc}")
@@ -210,8 +217,107 @@ async def document(doc: str):
     if not os.path.exists(path):
         return Response(content=messages.RESOURCE_NOT_FOUND, status_code=404)
     
-    log('docs', page_id=doc)
-    web_ui.pages.render_document(session_settings(), logged_in_user(), path)        
+    log_app('docs', page_id=doc)
+    web_ui.pages.render_document(session_settings(), logged_in_user(), path)      
+
+
+##### API SECTION #####
+@app.get("/api/beans", response_model=list[Bean])
+async def get_beans(
+    url: list[str] | None = Query(max_length=config.MAX_LIMIT, default=None),
+    tag: list[str] | None = Query(max_length=config.MAX_LIMIT, default=None),
+    kind: list[str] | None = Query(max_length=config.MAX_LIMIT, default=None), 
+    source: list[str] = Query(max_length=config.MAX_LIMIT, default=None),
+    ndays: int | None = Query(ge=config.MIN_WINDOW, le=config.MAX_WINDOW, default=None), 
+    start: int | None = Query(ge=0, default=0), 
+    limit: int | None = Query(ge=config.MIN_LIMIT, le=config.MAX_LIMIT, default=config.MAX_LIMIT)):
+    """
+    Retrieves the bean(s) with the given URL(s).
+    """
+    res = beanops.get(url, tag, kind, source, ndays, start, limit)  
+    log_api('get_beans', url=url, tag=tag, kind=kind, source=source, ndays=ndays, start=start, limit=limit, num_items=len(res) if res else None)
+    # return respond(res, "No beans found")
+    return res
+
+@app.get("/api/beans/search", response_model=list[Bean])
+async def search_beans(
+    q: str = None, 
+    acc: float = Query(ge=0, le=1, default=config.DEFAULT_ACCURACY),
+    tag: list[str] | None = Query(max_length=config.MAX_LIMIT, default=None),
+    kind: list[str] | None = Query(max_length=config.MAX_LIMIT, default=None), 
+    source: list[str] = Query(max_length=config.MAX_LIMIT, default=None),
+    ndays: int | None = Query(ge=config.MIN_WINDOW, le=config.MAX_WINDOW, default=None), 
+    start: int | None = Query(ge=0, default=0), 
+    limit: int | None = Query(ge=config.MIN_LIMIT, le=config.MAX_LIMIT, default=config.DEFAULT_LIMIT)):
+    """
+    Search beans by various parameters.
+    q: query string
+    acc: accuracy
+    tags: list of tags
+    kinds: list of kinds
+    source: list of sources
+    ndays: last n days
+    start: start index
+    limit: limit
+    """
+    res = beanops.search(q, acc, tag, kind, source, ndays, start, limit)
+    log_api('search_beans', q=q, acc=acc, tag=tag, kind=kind, source=source, ndays=ndays, start=start, limit=limit, num_items=len(res) if res else None)
+    # return respond(res, "No beans found")
+    return res
+@app.get("/api/beans/unique", response_model=list[Bean])
+async def unique_beans(
+    tag: list[str] | None = Query(max_length=config.MAX_LIMIT, default=None),
+    kind: list[str] | None = Query(default=None), 
+    source: list[str] = Query(max_length=config.MAX_LIMIT, default=None),
+    ndays: int | None = Query(ge=config.MIN_WINDOW, le=config.MAX_WINDOW, default=None), 
+    start: int | None = Query(ge=0, default=0), 
+    limit: int | None = Query(ge=config.MIN_LIMIT, le=config.MAX_LIMIT, default=config.MAX_LIMIT)):
+    """
+    Retuns a set of unique beans, meaning only one bean from each cluster will be included in the result.
+    To retrieve all the beans irrespective of cluster, use /beans endpoint.
+    To retrieve the beans related to the beans in this result set, use /beans/related endpoint.
+    """
+    res = beanops.unique(tag, kind, source, ndays, start, limit)
+    log_api('unique_beans', tag=tag, kind=kind, source=source, ndays=ndays, start=start, limit=limit, num_items=len(res) if res else None)
+    # return respond(res, "No beans found")
+    return res
+
+@app.get("/api/beans/related", response_model=list[Bean]|None)
+async def get_related_beans(
+    url: str, 
+    tag: list[str] | None = Query(max_length=config.MAX_LIMIT, default=None),
+    kind: list[str] | None = Query(default=None), 
+    source: list[str] = Query(max_length=config.MAX_LIMIT, default=None),
+    ndays: int | None = Query(ge=config.MIN_WINDOW, le=config.MAX_WINDOW, default=None), 
+    start: int | None = Query(ge=0, default=0), 
+    limit: int | None = Query(ge=config.MIN_LIMIT, le=config.MAX_LIMIT, default=config.MAX_LIMIT)):
+    """
+    Retrieves the related beans to the given bean.
+    """    
+    res = beanops.related(url, tag, kind, source, ndays, start, limit)
+    log_api('get_related_beans', url=url, tag=tag, kind=kind, source=source, ndays=ndays, start=start, limit=limit, num_items=len(res) if res else None)
+    # return respond(res, "No beans found")
+    return res
+
+@app.get("/api/beans/chatters", response_model=list[Chatter])
+async def get_chatters(url: list[str] | None = Query(max_length=config.MAX_LIMIT, default=None)):
+    """
+    Retrieves the latest social media stats for the given bean(s).
+    """
+    res = beanops.chatters(url)
+    log_api('get_chatters', url=url, num_items=len(res) if res else None)
+    # return respond(res, "No chatters found")
+    return res
+
+@app.get("/api/beans/sources")
+async def get_sources():
+    """
+    Retrieves the list of sources.
+    """
+    res = beanops.sources()
+    log_api('get_sources', num_items=len(res) if res else None)
+    # return respond(res, "No sources found")  
+    return res
 
 def initialize_server():
     embedder = RemoteEmbeddings(env.llm_base_url(), env.llm_api_key(), env.embedder_model(), env.embedder_n_ctx()) \
