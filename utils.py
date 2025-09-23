3054f4c8956266437cc909fce2963ef88d345113
Soumit Salman Rahman <soumitsr@gmail.com>
2025-09-23 20:36:37 +0000

    minor utils

--- utils.py
+++ utils.py
@@ -6,3 +6,4 @@ CLUSTER_EPS = float(os.getenv('CLUSTER_EPS', 0.3))
 
 now = lambda: datetime.now(timezone.utc)
 ndays_ago = lambda ndays: now() - timedelta(days=ndays)
+ndays_ago_str = lambda ndays: ndays_ago(ndays).strftime('%Y-%m-%d')
