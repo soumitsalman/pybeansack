577b89c6de0776026b305a5335e6e089f04c4bb3
Soumit Salman Rahman <soumitsr@gmail.com>
2025-10-22 21:55:43 +0000

    refactor: streamline query functions and remove unused S3 configurations

--- warehouse_readonly.py
+++ warehouse_readonly.py
@@ -10,6 +10,9 @@ from datetime import datetime
 from retry import retry
 from .models import *
 from .utils import *
+from icecream import ic
+
+DIGEST_COLUMNS = [K_URL, K_CREATED, K_GIST, K_CATEGORIES, K_SENTIMENTS]
 
 SQL_INIT = """
 INSTALL ducklake;
@@ -19,15 +22,6 @@ LOAD httpfs;
 INSTALL postgres;
 LOAD postgres;
 
-CREATE OR REPLACE SECRET s3secret (
-    TYPE s3,
-    PROVIDER config,
-    ENDPOINT '{s3_endpoint}',
-    REGION '{s3_region}',
-    KEY_ID '{s3_access_key_id}',
-    SECRET '{s3_secret_access_key}'
-);
-
 ATTACH 'ducklake:{catalog_path}' AS warehouse 
 (METADATA_SCHEMA 'beansack', DATA_PATH '{data_path}');
 USE warehouse;
@@ -35,10 +29,17 @@ USE warehouse;
 
 log = logging.getLogger(__name__)
 
-def _create_where_exprs(
+def _select(table: str, columns: list[str] = None, embedding: list[float] = None):
+    select_columns = columns.copy() if columns else ["*"]
+    if embedding: select_columns.append(f"array_cosine_distance(embedding::FLOAT[{len(embedding)}], ?::FLOAT[{len(embedding)}]) AS distance")
+    expr = f"SELECT {', '.join(select_columns)} FROM warehouse.{table}"
+    # if self.current_snapshot: expr += f" AT (VERSION => {self.current_snapshot})"
+    return expr
+
+def _where(
     kind: str = None,
     created: datetime = None,
-    collected: datetime = None,
+    updated: datetime = None,
     categories: list[str] = None,
     regions: list[str] = None,
     entities: list[str] = None,
@@ -50,7 +51,7 @@ def _create_where_exprs(
     params = []
     if kind: conditions.append("kind = ?"), params.append(kind)
     if created: conditions.append("created >= ?"), params.append(created)
-    if collected: conditions.append("collected >= ?"), params.append(collected)
+    if updated: conditions.append("updated >= ?"), params.append(updated)
     if categories: conditions.append("ARRAY_HAS_ANY(categories, ?)"), params.append(categories)
     if regions: conditions.append("ARRAY_HAS_ANY(regions, ?)"), params.append(regions)
     if entities: conditions.append("ARRAY_HAS_ANY(entities, ?)"), params.append(entities)
@@ -72,56 +73,34 @@ class Beansack:
         } 
 
         if catalogdb.startswith("postgresql://"): catalogdb = f"postgres:{catalogdb}"
-        s3_endpoint, s3_region, s3_access_key_id, s3_secret_access_key = "", "", "", ""
-        if storagedb.startswith("s3://"):
-            s3_endpoint = os.getenv('S3_ENDPOINT', '')
-            s3_region = os.getenv('S3_REGION', '')
-            s3_access_key_id = os.getenv('S3_ACCESS_KEY_ID', '')
-            s3_secret_access_key = os.getenv('S3_SECRET_ACCESS_KEY', '')
-        else: storagedb = os.path.expanduser(storagedb)
+        storagedb = os.path.expanduser(storagedb)
 
         init_sql = SQL_INIT.format(                
             catalog_path=catalogdb,
             data_path=storagedb,
-            # s3 storage configurations
-            s3_access_key_id=s3_access_key_id,
-            s3_secret_access_key=s3_secret_access_key,
-            s3_endpoint=s3_endpoint,
-            s3_region=s3_region,
         )
 
         self.db = duckdb.connect(config=config) 
         self.db.execute(init_sql)
-        log.debug("Data warehouse initialized.")        
-
-    def _select(self, table: str, columns: list[str] = None, embedding: list[float] = None):
-        columns = columns or ["*"]
-        if embedding: columns.append(f"array_cosine_distance(embedding::FLOAT[{VECTOR_LEN}], ?::FLOAT[{VECTOR_LEN}]) AS distance")
-        expr = f"SELECT {', '.join(columns)} FROM warehouse.{table}"
-        if self.current_snapshot: expr += f" AT (VERSION => {self.current_snapshot})"
-        return expr
+        log.debug("Data warehouse initialized.")       
     
-    def query_trending_beans(self, kind: str = None, created: datetime = None, categories: list[str] = None, regions: list[str] = None, entities: list[str] = None, sources: list[str] = None, embedding: list[float] = None, distance: float = 0, offset: int = 0, limit: int = 0, columns: list[str] = None):        
-        columns = columns or ["*"]
-        query_expr = self._select("trending_beans_view", columns, embedding)
-        conditions, params = _create_where_exprs(
-            kind=kind, created=created, categories=categories, regions=regions, entities=entities, sources=sources, distance=distance)
+    def query_trending_beans(self, kind: str = None, last_ndays: datetime = None, categories: list[str] = None, regions: list[str] = None, entities: list[str] = None, sources: list[str] = None, embedding: list[float] = None, distance: float = 0, offset: int = 0, limit: int = 0, columns: list[str] = None):        
+        query_expr = _select("trending_beans_view", columns, embedding)
+        conditions, params = _where(kind=kind, updated=last_ndays, categories=categories, regions=regions, entities=entities, sources=sources, distance=distance)
         if conditions: query_expr += " WHERE " + " AND ".join(conditions)
         if embedding: params = [embedding] + params
 
-        cursor = self.db.cursor()
+        cursor = self.db.cursor()        
         rel = cursor.query(query_expr, params=params)
         rel = rel.order("created DESC")
         if distance: rel = rel.order("distance ASC")
         if offset or limit: rel = rel.limit(limit, offset=offset)
         return [Bean(**dict(zip(rel.columns, row))) for row in rel.fetchall()]
 
-
-    def query_processed_beans(self, kind: str = None, created: datetime = None, categories: list[str] = None, regions: list[str] = None, entities: list[str] = None, sources: list[str] = None, embedding: list[float] = None, distance: float = 0, offset: int = 0, limit: int = 0, columns: list[str] = None):        
-        columns = columns or ["* EXCLUDE(title_length, summary_length, content_length)"]
-        query_expr = self._select("processed_beans_view", columns, embedding)
-        conditions, params = _create_where_exprs(
-            kind=kind, created=created, categories=categories, regions=regions, entities=entities, sources=sources, distance=distance)
+    def query_processed_beans(self, kind: str = None, last_ndays: datetime = None, categories: list[str] = None, regions: list[str] = None, entities: list[str] = None, sources: list[str] = None, embedding: list[float] = None, distance: float = 0, offset: int = 0, limit: int = 0, columns: list[str] = None):        
+        query_expr = _select("processed_beans_view", columns, embedding)
+        conditions, params = _where(
+            kind=kind, created=last_ndays, categories=categories, regions=regions, entities=entities, sources=sources, distance=distance)
         if conditions: query_expr += " WHERE " + " AND ".join(conditions)
         if embedding: params = [embedding] + params
 
@@ -134,7 +113,7 @@ class Beansack:
 
     def query_bean_chatters(self, collected: datetime, limit: int):        
         query_expr = self._select("bean_chatters_view")
-        conditions, params = _create_where_exprs(collected=collected)
+        conditions, params = _where(updated=collected)
         if conditions: query_expr = query_expr + " WHERE " + " AND ".join(conditions)
         cursor = self.db.cursor()
         rel = cursor.query(query_expr, params=params)
