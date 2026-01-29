3c1a167d367d0df315c6dcd0befd8bc128307b24
Soumit Salman Rahman <soumitsr@gmail.com>
2026-01-29 15:17:08 -0500

    feat: refactor CDNStore to use s3_link for S3 operations and enhance local file handling

--- cdnstore.py
+++ cdnstore.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 from .utils import random_filename
 
 def _ext_to_dir(ext: str) -> str:
@@ -14,7 +15,7 @@ class CDNStore:
     under the 'articles' folder by default (per your request images go to that
     folder as well).
     """
-    fs = None
+    s3_link = None
     root_path: str = None
     cdn_url: str = None
 
@@ -23,22 +24,20 @@ class CDNStore:
         root_path: str,        
         cdn_url: str = None
     ):  
-        is_s3 = root_path.startswith("s3://")
         self.root_path = root_path.removeprefix("s3://").removesuffix('/')
         if cdn_url: self.cdn_url = cdn_url.rstrip('/')
 
-        if is_s3:
-            from s3fs import S3FileSystem
-            region = os.getenv("S3_REGION")
-            self.fs = S3FileSystem(
-                endpoint_url=os.getenv("S3_ENDPOINT_URL"),
-                key=os.getenv("S3_ACCESS_KEY_ID"), 
-                secret=os.getenv("S3_SECRET_ACCESS_KEY"), 
-                client_kwargs={"region_name": region} if region else None,
-                config_kwargs={"s3": {"addressing_style": "virtual"}}
-            ) 
-        # else:
-        #     self.fs = os # use local filesystem
+        if not root_path.startswith("s3://"): return
+        
+        from s3fs import S3FileSystem
+        region = os.getenv("S3_REGION")
+        self.s3_link = S3FileSystem(
+            endpoint_url=os.getenv("S3_ENDPOINT"),
+            key=os.getenv("S3_ACCESS_KEY_ID"), 
+            secret=os.getenv("S3_SECRET_ACCESS_KEY"), 
+            client_kwargs={"region_name": region} if region else None,
+            config_kwargs={"s3": {"addressing_style": "virtual"}}
+        ) 
 
     def _relative_path(self, blob_name: str, ext: str) -> str:
         """Return s3fs-style path: 'bucket/key'"""
@@ -48,20 +47,31 @@ class CDNStore:
     def _public_url(self, key: str) -> str:
         return f"{self.cdn_url}/{key}"
 
-    def upload_file(self, source_file_path: str, directory: str = None) -> str:
-        from pathlib import Path
-        file_name = Path(source_file_path).name
-        file_path = f"{directory}/{file_name}" if directory else file_name
-        self.fs.put(source_file_path, f"{self.root_path}/{file_path}")
-        return self._public_url(file_path)
+    # def upload_file(self, source_file_path: str, directory: str = None) -> str:
+    #     from pathlib import Path
+    #     file_name = Path(source_file_path).name
+    #     file_path = f"{directory}/{file_name}" if directory else file_name
+    #     if self.s3_link:
+    #         self.s3_link.put(source_file_path, f"{self.root_path}/{file_path}")
+    #     return self._public_url(file_path)
     
-    def upload_binary(self, data: bytes, file_path: str = None) -> str:
-        with self.fs.open(f"{self.root_path}/{file_path}", "wb") as f:
-            f.write(data)
+    def upload_binary(self, data: bytes, file_path: str = None) -> str:   
+        if self.s3_link:
+            with self.s3_link.open(f"{self.root_path}/{file_path}", "wb") as f:
+                f.write(data)
+        else:
+            file = Path(self.root_path, file_path)
+            file.parent.mkdir(parents=True, exist_ok=True)
+            file.write_bytes(data)
         return self._public_url(file_path)
     
     def upload_text(self, data: str, file_path: str = None) -> str:
-        with self.fs.open(f"{self.root_path}/{file_path}", "w", encoding='utf-8') as f:
-            print(f.write(data))
+        if self.s3_link:
+            with self.s3_link.open(f"{self.root_path}/{file_path}", "w", encoding='utf-8') as f:
+                f.write(data)
+        else:
+            file = Path(self.root_path, file_path)
+            file.parent.mkdir(parents=True, exist_ok=True)
+            file.write_text(data, encoding='utf-8')
         return self._public_url(file_path)
 
